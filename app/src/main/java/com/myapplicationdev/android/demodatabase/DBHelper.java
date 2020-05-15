4742bd8942578f8e22ba99bb8b98d91dbcad087e
18016624 <18016624@myrp.edu.sg>
2020-05-15 15:52:50 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
@@ -0,0 +1,58 @@
+package com.myapplicationdev.android.demodatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VER = 1;
+    private static final String DATABASE_NAME = "tasks.db";
+
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_DATE = "date";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_TASK +  "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_DATE + " TEXT,"
+                + COLUMN_DESCRIPTION + " TEXT )";
+        db.execSQL(createTableSql);
+        Log.i("info" ,"created tables");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        // Create table(s) again
+        onCreate(db);
+    }
+
+    public void insertTask(String description, String date){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, description);
+        // Store the column name as key and the date as value
+        values.put(COLUMN_DATE, date);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
+    }
+
+}
